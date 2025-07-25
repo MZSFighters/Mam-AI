fba8ba5c51194199634387bdc5e6691a41058c06
Restioson <restiosondev@gmail.com>
2025-07-25 14:22:07 +0200

    refactor: split out RagStream into its own file

--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,19 +1,10 @@
 package com.example.app
 
-import android.app.Application
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import android.os.Handler
-import android.os.Looper
-import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.lifecycle.lifecycleScope
 import io.flutter.plugin.common.EventChannel
-import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
 
 class MainActivity : FlutterActivity() {
     private val channel = "io.github.mzsfighters.mam_ai/request_generation"
@@ -37,37 +28,3 @@ class MainActivity : FlutterActivity() {
     }
 }
 
-class RagStream(application: Application, val lifecycleScope: LifecycleCoroutineScope): EventChannel.StreamHandler {
-    private val backgroundExecutor: Executor = Executors.newSingleThreadExecutor()
-    private val executorService = Executors.newSingleThreadExecutor()
-
-    val ragPipeline: RagPipeline by lazy { RagPipeline(application) }
-    var events: EventChannel.EventSink? = null
-
-    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-        ragPipeline // Force lazy initialization to happen now
-        this.events = events
-    }
-
-    override fun onCancel(arguments: Any?) {}
-
-    fun generateResponse(prompt: String) {
-        executorService.submit {
-            lifecycleScope.launch {
-                withContext(backgroundExecutor.asCoroutineDispatcher()) {
-                    ragPipeline
-                        .generateResponse(prompt) { response, done ->
-                            // We are off the ui thread at the moment. You can only send
-                            // messages through event channels while on ui thread.
-                            // The `post` puts the sending part back onto the ui thread
-                            Handler(Looper.getMainLooper()).post {
-                                events?.success(
-                                    response.text
-                                )
-                            }
-                        }
-                }
-            }
-        }
-    }
-}
